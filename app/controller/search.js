99af03bd038a16f1a46a2a030af3868ebd290d65
JserShadow <424108968@qq.com>
2018-04-07 21:38:25 +0800

    add sort

--- app/controller/search.js
+++ app/controller/search.js
@@ -7,9 +7,9 @@ class SearchController extends Controller {
     const { content } = this.ctx.request.body;
     const { Jwcnotice, Jwcexam, Neaunotices } = this.ctx.model;
     const reg = new RegExp(content);
-    const jwcNoticesResult = await Jwcnotice.find({ title: reg });
-    const jwcExamResult = await Jwcexam.find({ title: reg });
-    const neauNoticeResult = await Neaunotices.find({ title: reg });
+    const jwcNoticesResult = await Jwcnotice.find({ title: reg }).sort({ createTime: -1 });
+    const jwcExamResult = await Jwcexam.find({ title: reg }).sort({ createTime: -1 });
+    const neauNoticeResult = await Neaunotices.find({ title: reg }).sort({ createTime: -1 });
     const totalnum = jwcExamResult.length + jwcNoticesResult.length + neauNoticeResult.length;
     this.ctx.body = {
       total: totalnum,
